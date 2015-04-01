840c234ba972b3f53c0ac887d67743ca866e7d3c
Geoff Buesing <gbuesing@gmail.com>
2015-03-31 20:30:40 -0500

    Add examples README

---
+++ examples/README.md
@@ -0,0 +1,31 @@
+# PCA Examples
+
+
+## Iris
+
+```./examples/iris.rb```
+
+Transforms 4-d iris dataset into 2-d and outputs plot (requires GNUPlot):
+
+![iris](https://raw.githubusercontent.com/gbuesing/pca/master/examples/data/iris.png)
+
+
+## Faces
+
+```./examples/faces.rb```
+
+Sample of original faces before running PCA:
+
+![iris](https://raw.githubusercontent.com/gbuesing/pca/master/examples/data/faces.png)
+
+Same sample faces, reconstructed from top 36 PCA components:
+
+![iris](https://raw.githubusercontent.com/gbuesing/pca/master/examples/data/faces_recovered.png)
+
+Same sample faces, reconstructed from top 36 PCA components:
+
+![iris](https://raw.githubusercontent.com/gbuesing/pca/master/examples/data/faces_recovered.png)
+
+36 top prinicpal components visualized:
+
+![iris](https://raw.githubusercontent.com/gbuesing/pca/master/examples/data/eigenfaces.png)
--- examples/faces.rb
+++ examples/faces.rb
@@ -1,57 +1,59 @@
 #!/usr/bin/env ruby
 
-# BEFORE RUNNING
-# download and resize faces via this script:
-# ./examples/utils/get_faces.rb
-
 require_relative '../lib/pca'
 require_relative 'utils/image_grid'
 require 'pnm'
 
-set_size = 100
+set_size = 10_000
 components = 36
 grid_size = 100
+data_dir = 'examples/data/faces'
+
+unless Dir.exists?(data_dir)
+  abort "Aborting. You need to run download script first:\n./examples/utils/get_faces.rb"
+end
+
+# LOAD IMAGES
 
 puts "Loading #{set_size} images..."
 
-face_files = Dir['examples/data/faces/*.pgm'].sample(set_size)
+face_files = Dir["#{data_dir}/*.pgm"].sample(set_size)
 
 images = face_files.map do |filename|
   image = PNM.read(filename)
   image.pixels.flatten
 end
 
+# OUTPUT ORIG IMAGES PNG
+
 orig_png = 'examples/out/faces.png'
 ImageGrid.new(images.slice(0, grid_size)).to_file orig_png
 `open #{orig_png}`
 
+# RUN PCA
 
 puts "Running PCA..."
 
 pca = PCA.new components: components
 t = Time.now
 transformed = pca.fit_transform images
-puts "PCA runtime: #{Time.now - t}"
+puts "PCA runtime: #{(Time.now - t).round(1)}s"
 
 puts "\nExplained_variance_ratio: #{pca.explained_variance_ratio.to_a.map {|v| v.round(4)}}"
 
-# puts "transform sample row:"
-# p transformed.row(0)
+# OUTPUT RECOVERED IMAGES PNG
 
-puts "\nReconstructing images from top 100 components..."
+puts "\nReconstructing images from top #{components} components..."
 t = Time.now
 recovered = pca.inverse_transform transformed
-puts "Inverse PCA runtime: #{Time.now - t}"
+puts "Inverse PCA runtime: #{(Time.now - t).round(1)}s"
 
-# puts "recovered sample row:"
-# p recovered.row(0)
 
 recovered_png = 'examples/out/faces_recovered.png'
 ImageGrid.new(recovered.to_a.slice(0, grid_size)).to_file recovered_png
 `open #{recovered_png}`
 
-# puts "components sample row shape=#{pca.components.shape.inspect}:"
-# p pca.components.row(0)
+# OUTPUT EIGENFACES PNG
 
 eigenfaces = pca.components.transpose
 
